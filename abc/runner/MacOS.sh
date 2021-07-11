b19db66a77e25311cf5d5fceef41d134512f3225
LyricWulf <lyricwulf@gmail.com>
2021-07-11 02:25:10 -0500

    a

--- abc/runner/MacOS.sh
+++ abc/runner/MacOS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Updating brew..."
 brew update
-echo "Installing macOS dependencies:"
+echo "Installing MacOS dependencies:"
 echo "  $ABC_DEPS"
 # Brew version syntax is already libxyz@1.2.3 (same as ours)
 brew install $ABC_DEPS
