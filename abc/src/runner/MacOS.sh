936023a9d7d77fa1dfaeea3dd37c5e8b7ccc70e4
LyricWulf <lyricwulf@gmail.com>
2021-11-02 21:42:43 -0500

    test no brew update :0

--- abc/src/runner/MacOS.sh
+++ abc/src/runner/MacOS.sh
@@ -7,4 +7,4 @@ fi
 echo "Installing MacOS dependencies:"
 echo "  $ABC_DEPS"
 # Brew version syntax is already libxyz@1.2.3 (same as ours)
-brew install $ABC_DEPS
+HOMEBREW_NO_AUTO_UPDATE=1 command brew install $ABC_DEPS
