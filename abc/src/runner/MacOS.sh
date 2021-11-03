62631f020f2add4b8f85f5879e0d7011f71d56b8
LyricWulf <lyricwulf@gmail.com>
2021-11-02 22:01:16 -0500

    a

--- abc/src/runner/MacOS.sh
+++ abc/src/runner/MacOS.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-if [ "$ABC_CFG_AUTO_UPDATE" == "true" ] 
+echo "Updating brew..."
+brew update
+
+echo "Upgrading all brew packages..."
+if [ "$ABC_CFG_UPGRADE_ALL" == "true" ] 
 then
-    echo "Updating brew..."
-    brew update
+    brew upgrade
 fi
+
 echo "Installing MacOS dependencies:"
 echo "  $ABC_DEPS"
+# Install brew deps. Reason for the NO_AUTO_UPDATE is the below discussion
+# https://github.com/Homebrew/discussions/discussions/57
 # Brew version syntax is already libxyz@1.2.3 (same as ours)
 HOMEBREW_NO_AUTO_UPDATE=1 command brew install $ABC_DEPS
